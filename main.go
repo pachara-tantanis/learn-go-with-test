4621fae44648a0ff15f2ca8817cce10e4b0399e5
Pachara Tantanis <pachara.tantanis@thoughtworks.com>
2020-07-14 15:31:34 +0700

    refactor

--- main.go
+++ main.go
@@ -20,13 +20,14 @@ func Hello(name string, language string) string {
 	return greetingPrefix(language) + name
 }
 
-func greetingPrefix(language string) string {
-	prefix := englishHelloPrefix
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix
+	return
 }
