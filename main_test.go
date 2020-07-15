bdb1d130a888b47ec4b396fdbade128892aaec36
Pachara Tantanis <pachara.tantanis@thoughtworks.com>
2020-07-15 11:04:47 +0700

    extract slice example to slice package

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -38,51 +37,5 @@ func TestHello(t *testing.T) {
 		got := Hello("Cal", "French")
 		want := "Bonjour, Cal"
 		assertCorrectMessage(t, got, want)
-	})
-}
-
-func TestSum(t *testing.T) {
-
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-
-		got := Sum(numbers)
-		want := 6
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
-}
-
-func TestSumAll(t *testing.T) {
-
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
-}
-
-func TestSumAllTails(t *testing.T) {
-	checksum := func(t *testing.T, got []int, want []int) {
-		t.Helper()
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
-	}
-	t.Run("make the sums of some slices", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{0, 9})
-		want := []int{2, 9}
-
-		checksum(t, got, want)
-	})
-
-	t.Run("safely sum empty slices", func(t *testing.T) {
-		got := SumAllTails([]int{}, []int{3, 4, 5})
-		want := []int{0, 9}
-
-		checksum(t, got, want)
 	})
 }
