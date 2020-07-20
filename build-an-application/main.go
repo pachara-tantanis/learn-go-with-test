200d5dfd768a7dc8a9234e36b49fedf1023cd8c1
Pachara Tantanis <pachara.tantanis@thoughtworks.com>
2020-07-20 10:54:34 +0700

    finished get score

--- build-an-application/main.go
+++ build-an-application/main.go
@@ -4,16 +4,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(w, score)
+}
+
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
-	if err := http.ListenAndServe(":5000", handler); err != nil {
+	server := &PlayerServer{store: &InMemoryPlayerStore{}}
+	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
 
+func GetPlayerScore(name string) int {
+	if name == "Pepper" {
+		return 20
+	}
 
-func PlayerServer(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "20")
+	if name == "Floyd" {
+		return 10
+	}
+
+	return 0
 }
