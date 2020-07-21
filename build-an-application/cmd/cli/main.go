3ad8d9150cef54631b74ea68f570e21403d6b016
Pachara Tantanis <pachara.tantanis@thoughtworks.com>
2020-07-21 17:17:49 +0700

    added scheduled alert

--- build-an-application/cmd/cli/main.go
+++ build-an-application/cmd/cli/main.go
@@ -25,6 +25,6 @@ func main() {
 		log.Fatalf("problem creating file system player store, %v ", err)
 	}
 
-	game := poker.NewCLI(store, os.Stdin)
+	game := poker.NewCLI(store, os.Stdin, )
 	game.PlayPoker()
 }
